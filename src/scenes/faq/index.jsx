ec9c2b0ace09a06ed960dbaf5d357f184c91fa0d
sandor <sandor.kornhauser@gmail.com>
2023-04-13 10:52:24 -0600

    Add faq scene

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,80 @@
+import { Box, useTheme, Typography } from '@mui/material';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { tokens } from '../../theme';
+
+import Header from '../../components/Header';
+
+const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="10px">
+        <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    An important question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci veritatis totam? Blanditiis molestiae consequuntur perspiciatis eveniet natus doloremque nostrum!
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Another important question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci veritatis totam? Blanditiis molestiae consequuntur perspiciatis eveniet natus doloremque nostrum!
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Your favorite question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci veritatis totam? Blanditiis molestiae consequuntur perspiciatis eveniet natus doloremque nostrum!
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    Some random question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci veritatis totam? Blanditiis molestiae consequuntur perspiciatis eveniet natus doloremque nostrum!
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                <Typography color={colors.greenAccent[500]} variant="h5">
+                    The final question
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci veritatis totam? Blanditiis molestiae consequuntur perspiciatis eveniet natus doloremque nostrum!
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+    </Box>
+  )
+}
+
+export default FAQ

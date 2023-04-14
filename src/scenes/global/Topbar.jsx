b285aab140fc459c43282e07268153e5c3388db1
sandor <sandor.kornhauser@gmail.com>
2023-04-14 12:49:07 -0600

    Finished dashboard

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -32,9 +32,9 @@ const Topbar = () => {
         <Box display="flex">
             <IconButton onClick={colorMode.toggleColorMode}>
                 {theme.palette.mode === 'dark' ? (
-                    <DarkModeOutlinedIcon/>
+                    <LightModeOutlinedIcon/>
                 ) :
-                (<LightModeOutlinedIcon/>)}
+                (<DarkModeOutlinedIcon/>)}
             </IconButton>
             <IconButton>
                 <NotificationsOutlinedIcon/>
